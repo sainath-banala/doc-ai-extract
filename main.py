04d6489d7f073e111eb0d91c570ce2856b263fd9
sainath-banala <longwaytogo45@gmail.com>
2026-03-17 11:22:57 +0530

    Adding extract-info endpoint, which extracts specific keywords and return it in json format

--- main.py
+++ main.py
@@ -27,3 +27,10 @@ async def analyze(file: UploadFile = File(...), question: str = Form(...)):
     pdf_content = parse_pdf(contents)
     response = ask_question_to_gemini(f"{question}: {pdf_content}")
     return {"file_name": {file.filename}, "size": len(contents), "response": response}
+
+@app.post("/extract-info")
+async def extract_info(file: UploadFile = File(..., description="Upload any PDF document"), keywords: str = Form(..., description="Comma separated keywords to extract. Example: name, value1, value2")):
+    contents = await file.read()
+    pdf_content = parse_pdf(contents)
+    response = ask_question_to_gemini(f"Extract the following keywords {keywords} from the document. Return only valid JSON with these exact keys. If a keyword is not found set its value to null. No explanation, no markdown, no code blocks: {pdf_content}")
+    return {"file_name": {file.filename}, "size": len(contents), "response": response.replace("```json", "").replace("```", "")}
